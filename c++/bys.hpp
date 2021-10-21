c09db4ddf738eee15ab87c5d980be68ec1726e8c
Masaki Kobayashi <bayashi.cl@gmail.com>
2021-10-21 11:09:01 +0900

    update template

--- c++/bys.hpp
+++ c++/bys.hpp
@@ -1,9 +1,15 @@
 #pragma once
+#ifndef LOCAL
+#define NDEBUG
+#endif
 
+#include <algorithm>
+#include <array>
 #include <atcoder/math>
 #include <atcoder/modint>
-#include <boost/range/irange.hpp>
+#include <cassert>
 #include <cmath>
+#include <complex>
 #include <iomanip>
 #include <iostream>
 #include <iterator>
@@ -14,29 +20,35 @@
 #include <set>
 #include <stack>
 #include <string>
-#include <valarray>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
 namespace bys {
 using atcoder::pow_mod, atcoder::inv_mod;
-using boost::irange;
+using std::array, std::vector, std::string, std::set, std::map, std::pair;
 using std::cin, std::cout, std::endl;
-using std::min, std::max, std::sort, std::abs, std::pow;
-using std::vector, std::string, std::set, std::map, std::pair;
+using std::min, std::max, std::sort, std::reverse, std::abs, std::pow;
 
 // alias
-using mint = atcoder::modint1000000007;
+using mint = atcoder::modint998244353;
 using ll = long long int;
-using Pa = std::pair<int, int>;
-using Vec = std::vector<int>;
+using Pa = pair<int, int>;
+using Vec = vector<int>;
 using VecVec = std::vector<Vec>;
+template <class T>
+using uset = std::unordered_set<T>;
+template <class S, class T>
+using umap = std::unordered_map<S, T>;
 
 // const
-constexpr int MOD = 1000000007;
+constexpr int MOD = 998244353;
 constexpr int INF = std::numeric_limits<int>::max() / 2;
 constexpr ll LINF = std::numeric_limits<ll>::max() / 2;
 
 // I/O
+// modint
 inline std::istream& operator>>(std::istream& is, mint& m) {
     ll n;
     is >> n;
@@ -44,44 +56,51 @@ inline std::istream& operator>>(std::istream& is, mint& m) {
     return is;
 }
 inline std::ostream& operator<<(std::ostream& os, const mint& m) {
-    os << m.val();
-    return os;
+    return os << m.val();
 }
-template <class T>
-std::istream& operator>>(std::istream& is, vector<T>& vec) {
-    for (auto&& v : vec) is >> v;
+
+// pair
+template <class T, class U>
+std::istream& operator>>(std::istream& is, std::pair<T, U>& p) {
+    return is >> p.first >> p.second;
+}
+template <typename T, typename U>
+std::ostream& operator<<(std::ostream& os, const std::pair<T, U>& p) {
+    return os << p.first << " " << p.second;
+}
+
+// STL container
+struct is_container_impl {
+    template <typename T>
+    static auto check(T&& obj)
+        -> decltype(obj.begin(), obj.end(), std::true_type{});
+    template <typename T>
+    static auto check(...) -> std::false_type;
+};
+template <typename T>
+class is_container
+    : public decltype(is_container_impl::check<T>(std::declval<T>())) {};
+
+template <typename C,
+          typename std::enable_if<is_container<C>::value &&
+                                      !std::is_same<C, std::string>::value &&
+                                      !std::is_same<C, std::wstring>::value,
+                                  std::nullptr_t>::type = nullptr>
+std::ostream& operator<<(std::ostream& os, const C& container) noexcept {
+    std::for_each(std::begin(container), std::prev(std::end(container)),
+                  [&](auto e) { os << e << ' '; });
+    return os << *std::prev(std::end(container));
+}
+
+template <typename C,
+          typename std::enable_if<is_container<C>::value &&
+                                      !std::is_same<C, std::string>::value &&
+                                      !std::is_same<C, std::wstring>::value,
+                                  std::nullptr_t>::type = nullptr>
+std::istream& operator>>(std::istream& is, C& container) {
+    std::for_each(std::begin(container), std::end(container),
+                  [&](auto&& e) { is >> e; });
     return is;
-}
-template <class T>
-std::ostream& operator<<(std::ostream& os, const vector<T>& vec) {
-    for (std::size_t i = 0; i < vec.size(); i++) {
-        if (i) os << " ";
-        os << vec[i];
-    }
-    return os;
-}
-template <class T>
-std::istream& operator>>(std::istream& is, std::valarray<T>& arr) {
-    for (auto&& a : arr) is >> a;
-    return is;
-}
-template <class T>
-std::ostream& operator<<(std::ostream& os, const std::valarray<T>& arr) {
-    for (std::size_t i = 0; i < arr.size(); i++) {
-        if (i) os << " ";
-        os << arr[i];
-    }
-    return os;
-}
-template <class S, class T>
-std::istream& operator>>(std::istream& is, std::pair<S, T>& p) {
-    is >> p.first >> p.second;
-    return is;
-}
-template <class S, class T>
-std::ostream& operator<<(std::ostream& os, const std::pair<S, T>& p) {
-    os << p.first << " " << p.second;
-    return os;
 }
 
 // I/O helper
@@ -94,33 +113,40 @@ inline T input() {
 template <class T>
 inline vector<T> input(int n) {
     vector<T> res(n);
-    for (auto&& r : res) cin >> r;
+    cin >> res;
     return res;
 }
 template <class T, size_t N>
 inline std::array<T, N> input() {
     std::array<T, N> res;
-    for (auto&& r : res) cin >> r;
+    cin >> res;
     return res;
 }
-template <class Tail>
-void print(Tail&& tail) {
-    cout << tail << "\n";
+template <class S, class T, class... Us>
+std::tuple<S, T, Us...> input() {
+    std::tuple<S, T, Us...> res;
+    std::apply([](auto&... e) { (cin >> ... >> e); }, res);
+    return res;
 }
-template <class Head, class... Body>
-void print(Head&& head, Body&&... tail) {
-    cout << head << " ";
-    print(std::forward<Body>(tail)...);
-}
-template <class Tail>
-void debug(Tail&& tail) {
-    std::clog << tail << endl;
-}
-template <class Head, class... Body>
-void debug(Head&& head, Body&&... tail) {
-    std::clog << head << " ";
-    debug(std::forward<Body>(tail)...);
-}
+
+struct Print {
+    std::ostream& os;
+    char sep = ' ', end = '\n';
+    Print() : os(std::cout) {}
+    Print(std::ostream& os) : os(os) {}
+    ~Print() { os << std::flush; }
+    void operator()() { os << end; }
+    template <class T>
+    void operator()(const T& a) {
+        os << a << end;
+    }
+    template <class T, class... Ts>
+    void operator()(const T& a, const Ts&... b) {
+        os << a;
+        (os << ... << (os << sep, b));
+        os << end;
+    }
+} print, debug(std::cerr);
 
 // utility
 template <class T>
@@ -139,12 +165,29 @@ inline bool chmin(T& a, const T& b) {
     }
     return 0;
 }
+template <class T>
+inline T ceil(T a, T b) {
+    return (a + b - 1) / b;
+}
+inline bool pop(int s, int d) { return s & (1 << d); }
 
 // config
 inline void init() {
     cin.tie(nullptr);
     std::ios::sync_with_stdio(false);
-    cout << std::fixed << std::setprecision(11) << std::boolalpha;
+    cout << std::fixed << std::setprecision(11);
+    std::cerr << std::boolalpha;
 }
 
+// macro
+#ifdef LOCAL
+#define DEBUG(...) debug("[debug] line:", __LINE__, "\n", __VA_ARGS__)
+#else
+#define DEBUG(...)
+#endif
+#define EXIT(...)           \
+    {                       \
+        print(__VA_ARGS__); \
+        return;             \
+    }
 }  // namespace bys
--- c++/test.cpp
+++ c++/test.cpp
@@ -1,11 +1,10 @@
-// #include "geometry.hpp"
-#include "geometry.hpp"
+#include "bys.hpp"
 
 using namespace bys;
+void solve() {}
 
 int main() {
-    init();
-    geo::Point a(3, 4);
-    geo::Point b(2, 1);
-    print(geo::len(a));
+    bys::init();
+    solve();
+    return 0;
 }
