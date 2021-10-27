1ca91e8d47c00028a81a67c65c05bd70c7b136c7
Masaki Kobayashi <bayashi.cl@gmail.com>
2021-10-28 02:26:56 +0900

    add lineralgebla

--- c++/linalg.hpp
+++ c++/linalg.hpp
@@ -3,49 +3,79 @@
 #include "bys.hpp"
 
 namespace bys {
-namespace linalg {
-const double EPS = 1e-10;
-int sgn(double a) { return (a < -EPS) ? -1 : (a > EPS) ? 1 : 0; }
-bool isclose(double a, double b) { return sgn(a - b) == 0; }
+template <class T>
+struct Matrix {
+    vector<vector<T>> mat;
+    Matrix(int i, int j) : mat(i, vector<T>(j)) {}
 
-struct Point {
-    double x, y;
-    Point() : x(0.0), y(0.0) {}
-    Point(double x, double y) : x(x), y(y) {}
-    Point(pair<double, double>& p) : x(p.first), y(p.second) {}
-    // clang-format off
-    Point operator+() const { return *this; }
-    Point operator+(const Point& other) const { return {x + other.x, y + other.y}; }
-    Point operator+(const double other) const { return {x + other, y + other}; }
-    Point& operator+=(const Point& other) { x += other.x; y += other.y; return *this; }
-    Point& operator+=(const double other) { x += other; y += other; return *this; }
-    Point operator-() const { return {-x, -y}; }
-    Point operator-(const Point& other) const { return {x - other.x, y - other.y}; }
-    Point operator-(const double other) const { return {x - other, y - other}; }
-    Point& operator-=(const Point& other) { x -= other.x; y -= other.y; return *this; }
-    Point& operator-=(const double other) { x -= other; y -= other; return *this; }
-    Point operator*(double other) const { return {x * other, y * other}; }
-    Point& operator*=(double other) { x *= other; y *= other; return *this; }
-    Point operator/(double other) const { return {x / other, y / other}; }
-    Point& operator*=(double other) { x /= other; y /= other; return *this; }
-    bool operator==(const Point& other) const { return isclose(x, other.x) && isclose(y, other.y); }
-    bool operator!=(const Point& other) const { return !(*this==other);}
-    // clang-format on
+    vector<T>& operator[](int k) { return mat[k]; }
+    int row() const { return mat.size(); }
+    int col() const { return mat[0].size(); }
+    pair<int, int> shape() const { return {row(), col()}; }
 
-    double norm2() const { return x * x + y * y; }
-    double norm() const { return std::sqrt(norm2()); }
-    Point normalized() const { return *this / norm(); }
+    Matrix operator+(const Matrix<T>& rh) {
+        assert(shape() == rh.shape());
+        auto [r, c] = shape();
+        Matrix<T> res(r, c);
+        for (int i = 0; i < r; ++i) {
+            for (int j = 0; j < c; ++j) {
+                res[i][j] = mat[i][j] + rh.mat[i][j];
+            }
+        }
+        return res;
+    }
+    Matrix operator-(const Matrix<T>& rh) {
+        assert(shape() == rh.shape());
+        auto [r, c] = shape();
+        Matrix<T> res(r, c);
+        for (int i = 0; i < r; ++i) {
+            for (int j = 0; j < c; ++j) {
+                res[i][j] = mat[i][j] - rh.mat[i][j];
+            }
+        }
+        return res;
+    }
+    Matrix operator*(const T rh) {
+        auto [r, c] = shape();
+        Matrix<T> res(r, c);
+        for (int i = 0; i < r; ++i) {
+            for (int j = 0; j < c; ++j) {
+                res[i][j] = mat[i][j] * rh;
+            }
+        }
+        return res;
+    }
+    Matrix operator/(const T rh) {
+        auto [r, c] = shape();
+        Matrix<T> res(r, c);
+        for (int i = 0; i < r; ++i) {
+            for (int j = 0; j < c; ++j) {
+                res[i][j] = mat[i][j] / rh;
+            }
+        }
+        return res;
+    }
+
+    Matrix operator*(const Matrix<T>& rh) {
+        assert(col() == rh.row());
+        auto [r, m] = shape();
+        int c = rh.col();
+        Matrix<T> res(r, c);
+        for (int i = 0; i < r; ++i) {
+            for (int j = 0; j < c; ++j) {
+                for (int k = 0; k < m; ++k) {
+                    res.mat[i][j] += mat[i][k] * rh.mat[k][j];
+                }
+            }
+        }
+    }
+
+    template <class T>
+    static Matrix<T> ident(int n) {
+        Matrix res(n, n);
+        for (int i = 0; i < n; ++i) mat[i][i] = 1;
+        return res;
+    }
+    static Matrix<double> rotate(double theta) {}
 };
-inline bool operator<(const Point& a, const Point& b) {
-    if (sgn(a.x - b.x)) return sgn(a.x - b.x) < 0;
-    return sgn(a.y - b.y) < 0;
-}
-Point operator+(double lh, const Point& rh) { return {lh + rh.x, lh + rh.y}; };
-Point operator-(double lh, const Point& rh) { return {lh - rh.x, lh - rh.y}; };
-Point operator*(double lh, const Point& rh) { return {lh * rh.x, lh * rh.y}; };
-Point operator/(double lh, const Point& rh) { return {lh / rh.x, lh / rh.y}; };
-double dist(Point& lh, Point& rh) { return (rh - lh).norm(); }
-double dot(Point& lh, Point& rh) { return lh.x * lh.y + rh.x * rh.y; }
-
-}  // namespace linalg
 }  // namespace bys
