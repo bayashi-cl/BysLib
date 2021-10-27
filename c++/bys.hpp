cab7a54b8cb7126a2b1677fa2f32ea85228ca493
Masaki Kobayashi <bayashi.cl@gmail.com>
2021-10-28 02:25:55 +0900

    fix

--- c++/bys.hpp
+++ c++/bys.hpp
@@ -1,4 +1,5 @@
 #pragma once
+
 #ifndef LOCAL
 #define NDEBUG
 #endif
--- c++/test.cpp
+++ c++/test.cpp
@@ -1,20 +1,11 @@
 #include "bys.hpp"
-#include "geo.hpp"
+#include "linalg.hpp"
 
 using namespace bys;
-void solve() {
-    using V = geo::Vec2d;
-    V a(0, 0);
-    V b(3, 0);
-    V c(5, 2);
-    V d(2, 2);
-
-    geo::Polygon tri = {a, b, c, d};
-    print(tri.area());
-}
-
 int main() {
-    bys::init();
-    solve();
+    Matrix<int> mat(2, 2);
+    mat[0][0] = 1;
+    print(mat[0][0]);
+
     return 0;
 }
