e53e4f1bd4d6bee51d91053046eaf4506b6c737d
Masaki Kobayashi <bayashi.cl@gmail.com>
2021-10-29 09:45:18 +0900

    add some method

--- c++/geo.hpp
+++ c++/geo.hpp
@@ -12,49 +12,51 @@ double radian(double degree) { return degree * (PI / 180.0); }
 double degree(double theta) { return theta * (180.0 / PI); }
 
 // ベクトル・点
-struct Vec2d {
+struct Point {
     double x, y;
-    Vec2d() : x(0.0), y(0.0) {}
-    Vec2d(double x, double y) : x(x), y(y) {}
-    std::istream& operator>>(std::istream& is) { return is >> x >> y; }
-    std::ostream& operator<<(std::ostream& os) { return os << x << ' ' << y; }
-
-    Vec2d operator+() const { return *this; }
-    Vec2d operator-() const { return Vec2d(-x, -y); }
-    Vec2d operator+(const Vec2d& rh) const { return Vec2d(x + rh.x, y + rh.y); }
-    Vec2d operator-(const Vec2d& rh) const { return Vec2d(x - rh.x, y - rh.y); }
-    Vec2d operator*(const double rh) const { return Vec2d(x * rh, y * rh); }
-    Vec2d operator/(const double rh) const { return Vec2d(x / rh, y / rh); }
+    Point() : x(0.0), y(0.0) {}
+    Point(double x, double y) : x(x), y(y) {}
     // clang-format off
-    Vec2d operator+=(const Vec2d& rh) { x += rh.x; y += rh.y; return *this; }
-    Vec2d operator-=(const Vec2d& rh) { x -= rh.x; y -= rh.y; return *this; }
-    Vec2d operator*=(const double rh) { x *= rh; y *= rh; return *this; }
-    Vec2d operator/=(const double rh) { x /= rh; y /= rh; return *this; }
-    bool operator==(const Vec2d& rh) const { return isclose(x, rh.x) && isclose(y, rh.y); }
-    bool operator!=(const Vec2d& rh) const { return !(*this == rh); }
+    friend std::istream& operator>>(std::istream& is, Point& v) { return is >> v.x >> v.y; }
+    friend std::ostream& operator<<(std::ostream& os, const Point& v) { return os << v.x << ' ' << v.y; }
+    Point operator+() const { return *this; }
+    Point operator-() const { return Point(-x, -y); }
+    Point operator+(const Point& rh) const { return Point(x + rh.x, y + rh.y); }
+    Point operator-(const Point& rh) const { return Point(x - rh.x, y - rh.y); }
+    Point operator*(const double rh) const { return Point(x * rh, y * rh); }
+    Point operator/(const double rh) const { return Point(x / rh, y / rh); }
+    Point operator+=(const Point& rh) { x += rh.x; y += rh.y; return *this; }
+    Point operator-=(const Point& rh) { x -= rh.x; y -= rh.y; return *this; }
+    Point operator*=(const double rh) { x *= rh; y *= rh; return *this; }
+    Point operator/=(const double rh) { x /= rh; y /= rh; return *this; }
+    bool operator==(const Point& rh) const { return isclose(x, rh.x) && isclose(y, rh.y); }
+    bool operator!=(const Point& rh) const { return !(*this == rh); }
     // clang-format on
 
     double norm2() const { return x * x + y * y; }
     double norm() const { return std::sqrt(norm2()); }
-    Vec2d normalized() const { return Vec2d(x / norm(), y / norm()); }
+    Point normalized() const { return Point(x / norm(), y / norm()); }
     double angle() const { return std::atan2(y, x); }
-    Vec2d rotate(double theta) {
+    Point rotate(double theta) {
         double ct = std::cos(theta), st = std::sin(theta);
-        return Vec2d(x * ct - y * st, x * st + y * ct);
+        return Point(x * ct - y * st, x * st + y * ct);
     }
-    Vec2d normal() const { return Vec2d(-normalized().y, normalized().x); }
-    double dot(const Vec2d& rh) const { return x * rh.x + y * rh.y; }
-    double det(const Vec2d& rh) const { return x * rh.y - y * rh.x; }
+    double dot(const Point& rh) const { return x * rh.x + y * rh.y; }
+    double det(const Point& rh) const { return x * rh.y - y * rh.x; }
+    Point normal() const { return Point(-normalized().y, normalized().x); }
+    Point projection(const Point& to) const {
+        return to * (dot(to) / to.norm2());
+    }
 };
 
-/*
-+1: ab->bcが左に曲がる
--1: ab->bcが右に曲がる
-+2: abcで直線上
--2: bacで直線上
- 0: acbで直線上
+/**
+ * +1: ab->bcが左に曲がる
+ * -1: ab->bcが右に曲がる
+ * +2: abcで直線上
+ * -2: bacで直線上
+ *  0: acbで直線上
  */
-int iSP(const Vec2d& a, const Vec2d& b, const Vec2d& c) {
+int iSP(const Point& a, const Point& b, const Point& c) {
     int flg = sgn((b - a).det(c - a));
     if (flg == 1) {
         return +1;
@@ -73,20 +75,20 @@ int iSP(const Vec2d& a, const Vec2d& b, const Vec2d& c) {
 
 // 直線・線分
 struct Line {
-    Vec2d p, q;
-    Line(Vec2d p, Vec2d q) : p(p), q(q) {}
-    Vec2d to_Vec2d() const { return q - p; }
-    Vec2d to_unit_Vec2d() const { return to_Vec2d().normalized(); }
-    double slope() const { return to_Vec2d().angle(); }
+    Point p, q;
+    Line(Point p, Point q) : p(p), q(q) {}
+    Point to_Point() const { return q - p; }
+    Point to_unit_Point() const { return to_Point().normalized(); }
+    double slope() const { return to_Point().angle(); }
     bool operator==(const Line& rh) const {
         return abs(iSP(p, q, rh.p)) != 1 && abs(iSP(p, q, rh.q)) != 1;
     }
     bool operator!=(const Line& rh) const { return !(*this == rh); }
 };
 struct Segment : Line {
-    using Line::Line;
-    double length() const { return to_Vec2d().norm(); }
-    Vec2d internal_division(double m, double n) const {
+    Segment(Point& p, Point& q) : Line::Line(p, q) {}
+    double length() const { return to_Point().norm(); }
+    Point internal_division(double m, double n) const {
         return (p * n + q * m) / (m + n);
     }
     bool operator==(const Segment& rh) const {
@@ -94,25 +96,81 @@ struct Segment : Line {
     }
     bool operator!=(const Segment& rh) const { return !(*this == rh); }
 };
+int iSP(const Point& p, const Line& l) { return iSP(l.p, l.q, p); }
 
-int iSP(const Segment& s, const Vec2d& c) { return iSP(s.p, s.q, c); }
-int iSP(const Line& l, const Vec2d& c) { return iSP(l.p, l.q, c); }
-
-bool cross(Line& a, Line& b) {
-    return sgn(a.to_Vec2d().det(b.to_Vec2d())) != 0;
+bool is_parallel(const Line& a, const Line& b) {
+    return sgn(a.to_Point().det(b.to_Point())) == 0;
 }
-bool cross(Segment& a, Segment& b) {
-    return iSP(a, b.p) * iSP(a, b.q) <= 0 && iSP(b, a.p) * iSP(b, a.q) <= 0;
+bool is_vertial(const Line& a, const Line& b) {
+    return sgn(a.to_Point().dot(b.to_Point())) == 0;
+}
+
+bool is_cross(const Line& a, const Line& b) {
+    return sgn(a.to_Point().det(b.to_Point())) != 0 ||
+           sgn(a.to_Point().det(b.p - a.p)) == 0;
+}
+
+bool is_cross(const Segment& a, const Segment& b) {
+    return iSP(b.p, a) * iSP(b.q, a) <= 0 && iSP(a.p, b) * iSP(a.q, b) <= 0;
+}
+
+double angle(const Line& a, const Line& b) {
+    return std::atan2(a.to_Point().det(b.to_Point()),
+                      a.to_Point().dot(b.to_Point()));
+}
+/**
+ * ∠abcが
+ * -1: 鈍角
+ *  0: 直角
+ * +1: 鋭角
+ */
+int angle_type(const Point& a, const Point& b, const Point& c) {
+    return sgn((a - b).dot(c - b));
+}
+
+double distance(const Point& p, const Line& l) {
+    return abs(l.to_Point().det(p - l.p) / (l.q - l.p).norm());
+}
+double distance(const Point& p, const Segment& s) {
+    if (angle_type(s.p, s.q, p) == -1) {
+        return (p - s.q).norm();
+    } else if (angle_type(s.q, s.p, p) == -1) {
+        return (p - s.p).norm();
+    } else {
+        return distance(p, Line(s.p, s.q));
+    }
+}
+double distance(const Segment& s, const Segment& t) {
+    if (is_cross(s, t)) return 0;
+
+    return min({distance(s.p, t), distance(s.q, t), distance(t.p, s),
+                distance(t.q, s)});
+}
+
+std::pair<bool, Point> cross_point(const Line& a, const Line& b) {
+    if (!is_cross(a, b)) return {false, Point()};
+    Point s = b.p - a.p;
+    Point res = b.p + b.to_Point() * abs(a.to_Point().det(s) /
+                                         a.to_Point().det(b.to_Point()));
+    return {true, res};
+}
+
+Point projection(const Point& p, const Line& l) {
+    return (p - l.p).projection(l.to_Point()) + l.p;
+}
+Point reflection(const Point& p, const Line& l) {
+    return p + (projection(p, l) - p) * 2;
 }
 
 // 多角形
 struct Polygon {
-    std::vector<Vec2d> vertex;
-    Polygon(std::initializer_list<Vec2d> init)
-        : vertex(init.begin(), init.end()) {}
-    Polygon(std::vector<Vec2d> vertex) : vertex(vertex) {}
+    int n_vertex;
+    std::vector<Point> vertex;
+    Polygon(std::initializer_list<Point> init)
+        : n_vertex(init.size()), vertex(init.begin(), init.end()) {}
+    Polygon(std::vector<Point> vertex)
+        : n_vertex(vertex.size()), vertex(vertex) {}
     double area() const {
-        int n_vertex = vertex.size();
         if (n_vertex < 3) return 0;
         double s = 0.0;
         for (int i = 0; i < n_vertex; ++i) {
@@ -120,6 +178,17 @@ struct Polygon {
         }
         return s * 0.5;
     };
+    bool is_convex() const {
+        int left = 0;
+        int right = 0;
+        for (int i = 0; i < n_vertex; ++i) {
+            int res = iSP(vertex[i], vertex[(i + 1) % n_vertex],
+                          vertex[(i + 2) % n_vertex]);
+            if (res == -1) left++;
+            if (res == 1) right++;
+        }
+        return left == 0 || right == 0;
+    }
 };
 }  // namespace geo
 }  // namespace bys
--- c++/test.cpp
+++ c++/test.cpp
@@ -1,11 +1,14 @@
 #include "bys.hpp"
-#include "linalg.hpp"
+#include "geo.hpp"
+// #include "linalg.hpp"
 
 using namespace bys;
 int main() {
-    Matrix<int> mat(2, 2);
-    mat[0][0] = 1;
-    print(mat[0][0]);
-
+    geo::Vec2d a(0, 0);
+    geo::Vec2d b(1, 1);
+    geo::Segment s(a, b);
+    geo::Segment t(b, a);
+    print(s == t);
+    // print(s.length());
     return 0;
 }
