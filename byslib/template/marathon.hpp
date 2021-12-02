77316a7600ce54057ab572cd14631d0fe5114422
Masaki Kobayashi <bayashi.cl@gmail.com>
2021-12-02 17:33:57 +0900

    timer 追加

---
+++ byslib/template/marathon.hpp
@@ -0,0 +1,13 @@
+#include <chrono>
+
+namespace bys {
+struct Timer {
+    std::chrono::time_point<std::chrono::system_clock> end;
+    Timer(int ms) {
+        end = std::chrono::system_clock::now() + std::chrono::milliseconds(ms);
+    }
+    inline bool counting() const {
+        return std::chrono::system_clock::now() <= end;
+    }
+};
+}  // namespace bys
