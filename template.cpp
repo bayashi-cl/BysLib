a9165c06438e326d0fcbac1d698613cb1659c545
Masaki Kobayashi <bayashi.cl@gmail.com>
2021-12-02 17:53:39 +0900

    テンプレ移動

---
+++ template.cpp
@@ -0,0 +1,13 @@
+#include "byslib/template/bys.hpp"
+
+namespace bys {
+void Solver::solve() {
+    
+}
+}  // namespace bys
+
+int main() {
+    bys::Solver solver;
+    solver.solve(/* bys::input<int>() */);
+    return 0;
+}
