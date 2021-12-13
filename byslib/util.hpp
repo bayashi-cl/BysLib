995933ab4b76276efc9bdfa2b1236d86258924b2
Masaki Kobayashi <bayashi.cl@gmail.com>
2021-12-13 11:30:12 +0900

    座圧に解凍を追加

--- byslib/util.hpp
+++ byslib/util.hpp
@@ -52,17 +52,29 @@ double bisect_float(double ok, double ng, int rep, Lambda is_ok, Args... args) {
 template <class T>
 struct Compress {
     vector<T> cp;
+    bool build = false;
     Compress() {}
     Compress(vector<T>& vec) : cp(vec) {}
-    void add(T v) { cp.push_back(v); }
+    void add(T v) {
+        assert(!build);
+        cp.push_back(v);
+    }
     void construct() {
+        assert(!build);
         sort(std::begin(cp), std::end(cp));
         cp.erase(std::unique(std::begin(cp), std::end(cp)), cp.end());
+        build = true;
     }
-    int get(T v) {
+    int get(T v) const {
+        assert(build);
         auto itr = std::lower_bound(std::begin(cp), std::end(cp), v);
         assert(*itr == v);
         return std::distance(cp.begin(), itr);
+    }
+    int size() const { return cp.size(); }
+    T unzip(int i) {
+        assert(build);
+        return cp[i];
     }
 };
 
