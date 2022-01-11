006ba26ed832211a4a4685870207e310227e5c8d
Masaki Kobayashi <bayashi.cl@gmail.com>
2022-01-12 08:29:07 +0900

    修正

--- graph/bellman_ford.hpp
+++ graph/bellman_ford.hpp
@@ -6,26 +6,24 @@
 
 namespace bys {
 struct BellmanFord {
-    // using Edge = std::pair<ll, std::pair<int, int>>;
-    // vector<Edge> graph;
     const EdgeList& graph;
     int n_node;
     vector<ll> cost;
+    bool negative_cycle;
 
-    BellmanFord(vector<Edge> graph, int n_node, int start) : graph(graph), n_node(n_node), cost(n_node, INF) { search(start); }
+    BellmanFord(const EdgeList& graph, int n_node, int start) : graph(graph), n_node(n_node), cost(n_node, LINF) {
+        negative_cycle = search(start);
+    }
 
     bool search(int start) {
+        cost[start] = 0;
         for (int i = 0; i < n_node - 1; ++i) relaxing();
-        return !relaxing();
+        return relaxing();
     }
 
    private:
     bool relaxing() {
         bool res = false;
-        // for (auto [dist, node] : graph) {
-        //     if (cost[node.first] == LINF) continue;
-        //     if (chmin(cost[node.second], cost[node.first] + dist)) res = true;
-        // }
         for (auto&& e : graph) {
             if (cost[e.from] == LINF) continue;
             if (chmin(cost[e.to], cost[e.from] + e.cost)) res = true;
--- graph/edge.hpp
+++ graph/edge.hpp
@@ -13,7 +13,7 @@ struct Edge {
     //! @brief 重み付き両頂点
     Edge(int from, int to, ll cost) : from(from), to(to), cost(cost) {}
     bool operator<(const Edge& rh) const { return cost < rh.cost; }
-    friend std::ostream& operator>>(std::ostream& os, const Edge& e) { return os << e.from << " - " << e.to << ": " << e.cost; }
+    friend std::ostream& operator<<(std::ostream& os, const Edge& e) { return os << e.from << " - " << e.to << ": " << e.cost; }
 };
 using Adj = vector<vector<Edge>>;
 using EdgeList = vector<Edge>;
