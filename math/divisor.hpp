ab0fad74d64f02f119ffa5fd26f1d6b6b23a9681
Masaki Kobayashi <bayashi.cl@gmail.com>
2022-01-21 01:13:40 +0900

    バグ修正

--- math/divisor.hpp
+++ math/divisor.hpp
@@ -5,7 +5,7 @@ namespace bys {
 template <typename T>
 vector<T> make_divisor(T n) {
     vector<T> lower, upper;
-    for (int i = 1; i * i <= n; ++i) {
+    for (T i = 1; i * i <= n; ++i) {
         if (n % i == 0) {
             lower.push_back(i);
             if (T j = n / i; i != j) upper.push_back(j);
