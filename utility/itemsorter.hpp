d6a2629400a2689eb9fffc9d882e55b20fca6036
Masaki Kobayashi <bayashi.cl@gmail.com>
2022-02-09 20:17:34 +0900

    TupleSorter追加

--- utility/itemsorter.hpp
+++ utility/itemsorter.hpp
@@ -7,10 +7,16 @@ template <class T, std::size_t I>
 struct ItemSorter {
     bool operator()(const T& lh, const T& rh) { return lh[I] < rh[I]; }
 };
+//! @brief Tuple系のI番目の要素を比較
+template <class T, std::size_t I>
+struct TupleSorter {
+    bool operator()(const T& lh, const T& rh) { return std::get<I>(lh) < std::get<I>(rh); }
+};
 template <class T, class U>
 struct SecondSorter {
-    bool operator()(const pair<T, U>& lh, const pair<T, U>& rh) {
+    bool operator()(const std::pair<T, U>& lh, const std::pair<T, U>& rh) {
         return lh.second == rh.second ? lh.first < rh.first : lh.second < rh.second;
     }
 };
+#define SORTBY(method) [](const auto& a, const auto& b) { return a.method < b.method; }
 }  // namespace bys
