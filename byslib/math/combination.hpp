fddfae888d1dd15c84074cdf931f5913dba01d76
Masaki Kobayashi <bayashi.cl@gmail.com>
2022-11-28 09:56:23 +0900

    fact配列を自動で延長

--- byslib/math/combination.hpp
+++ byslib/math/combination.hpp
@@ -15,38 +15,43 @@ namespace bys {
  * クエリ O(1)
  * See: https://drken1215.hatenablog.com/entry/2018/06/08/210000
  */
-struct MultiComb {
-    i32 _n;
+class MultiComb {
     i32 mod;
-    std::vector<i64> fact, factinv, inv;
+    std::vector<i32> fact, factinv, inv;
 
-    MultiComb(i32 n, i32 mod = MOD) : _n(n), mod(mod) {
-        fact.resize(_n + 1);
-        factinv.resize(_n + 1);
-        inv.resize(_n + 1);
-
-        fact[0] = fact[1] = 1;
-        factinv[0] = factinv[1] = 1;
-        inv[1] = 1;
-
-        for (i32 i = 2; i <= _n; i++) {
-            fact[i] = fact[i - 1] * i % mod;
-            inv[i] = mod - inv[mod % i] * (mod / i) % mod;
-            factinv[i] = factinv[i - 1] * inv[i] % mod;
+    void extend(i32 n) {
+        if ((i32)fact.size() > n) return;
+        fact.resize(n + 1);
+        factinv.resize(n + 1);
+        inv.resize(n + 1);
+        for (i32 i = fact.size(); i <= n; ++i) {
+            fact[i] = (i64)fact[i - 1] * i % mod;
+            inv[i] = mod - (i64)inv[mod % i] * (mod / i) % mod;
+            factinv[i] = (i64)factinv[i - 1] * inv[i] % mod;
         }
     }
 
-    i64 fac(i32 n) const { return fact[n]; }
+  public:
+    MultiComb(i32 n = 1, i32 mod = MOD) : mod(mod), fact({1, 1}), factinv({1, 1}), inv({0, 1}) {
+        extend(n);
+    }
 
-    i64 comb(i32 n, i32 r) const {
-        if (r < 0 || n < r) return 0;
-        return fact[n] * (factinv[r] * factinv[n - r] % mod) % mod;
+    i32 fac(i32 n) {
+        extend(n);
+        return fact[n];
     }
-    i64 perm(i32 n, i32 r) const {
+
+    i32 comb(i32 n, i32 r) {
         if (r < 0 || n < r) return 0;
-        return fact[n] * factinv[n - r] % mod;
+        extend(n);
+        return (i64)fact[n] * ((i64)factinv[r] * factinv[n - r] % mod) % mod;
     }
-    i64 hom(i32 n, i32 r) const {
+    i32 perm(i32 n, i32 r) {
+        if (r < 0 || n < r) return 0;
+        extend(n);
+        return (i64)fact[n] * factinv[n - r] % mod;
+    }
+    i32 hom(i32 n, i32 r) {
         if (n == 0 && r == 0) return 1;
         return comb(n + r - 1, r);
     }
@@ -57,7 +62,7 @@ struct MultiComb {
  *
  * O(min(n, (n - r)))
  *
- * @param n Modi32も渡せる
+ * @param n Modintも渡せる
  */
 template <class T> constexpr T comb(T n, i32 r) {
     T num = 1, den = 1;
