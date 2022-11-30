5afd93ad9cffa2389292a374263a97982a4ff95f
Masaki Kobayashi <bayashi.cl@gmail.com>
2022-11-30 18:04:53 +0900

    move

--- byslib/ds/ordered_set.hpp
+++ byslib/ds/ordered_set.hpp
@@ -8,10 +8,7 @@
  */
 namespace bys {
 namespace pbds = __gnu_pbds;
-template <class T, class Comp>
-using Tree = pbds::
+template <class T, class Comp = std::less<T>>
+using OrderdSet = pbds::
     tree<T, pbds::null_type, Comp, pbds::rb_tree_tag, pbds::tree_order_statistics_node_update>;
-
-template <class T> using OrderdSet = Tree<T, std::less<T>>;
-template <class T> using OrderdSetGt = Tree<T, std::greater<T>>;
 }  // namespace bys
