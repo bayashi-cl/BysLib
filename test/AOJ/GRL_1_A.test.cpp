4bc79cfff59390b5bbad81c0ab28cc76c45c8b3d
Masaki Kobayashi <bayashi.cl@gmail.com>
2022-12-01 07:53:06 +0900

    add test

---
+++ test/AOJ/GRL_1_A.test.cpp
@@ -0,0 +1,26 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/5/GRL/all/GRL_1_A"
+#include "byslib/graph/dijkstra.hpp"
+#include "byslib/template.hpp"
+
+namespace bys {
+void Solver::solve() {
+    auto [v, e, r] = scanner.read<i32, 3>();
+
+    EList edges(v);
+    for (UV : irange(e)) {
+        auto [s, t, d] = scanner.read<i32, i32, i64>();
+        edges.add_edge(s, t, d);
+    }
+    auto graph = edges.adj();
+    auto res = dijkstra(graph, r);
+    for (auto ans : res) {
+        if (is_inf(ans)) {
+            print("INF");
+        } else {
+            print(ans);
+        }
+    }
+}
+}  // namespace bys
+
+int main() { return bys::Solver::main(/* bys::scanner.read<int>() */); }
---
+++ test/AOJ/GRL_1_C.test.cpp
@@ -0,0 +1,40 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/5/GRL/all/GRL_1_C"
+#include "byslib/graph/warshall_floyd.hpp"
+#include "byslib/template.hpp"
+
+namespace bys {
+void Solver::solve() {
+    auto [v, e] = scanner.read<i32, 2>();
+
+    EList edges(v);
+    for (UV : irange(e)) {
+        auto [s, t, d] = scanner.read<i32, i32, i64>();
+        edges.add_edge(s, t, d);
+    }
+    auto res = warshall_floyd(edges);
+    for (auto&& i : irange(v)) {
+        if (res[i][i] < 0) EXIT("NEGATIVE CYCLE");
+    }
+
+    for (auto&& ri : res) {
+        auto top = true;
+
+        for (auto&& ans : ri) {
+            if (top) {
+                top = false;
+            } else {
+                cout << ' ';
+            }
+            if (is_inf(ans)) {
+                cout << "INF";
+            } else {
+                cout << ans;
+            }
+        }
+        cout << '\n';
+    }
+}
+}  // namespace bys
+// NEGATIVE_CYCLE
+// NEGATIVE_CYCLE
+int main() { return bys::Solver::main(/* bys::scanner.read<int>() */); }
